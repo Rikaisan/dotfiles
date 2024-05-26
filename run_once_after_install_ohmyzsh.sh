829477b75400736177ece4273d9cabc31e797f24
Rikai <rikaisensei@gmail.com>
2024-05-25 20:28:23 -0500

    Better user feedback

--- run_once_after_install_ohmyzsh.sh
+++ run_once_after_install_ohmyzsh.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+PREFIX="[CHEZMOI]"
+
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
-	echo "Getting ohmyz.sh"
+	echo "$PREFIX Getting ohmyz.sh"
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
 else
-	echo ".oh-my-zsh already found, skipping."
+	echo "$PREFIX .oh-my-zsh already found, skipping..."
 fi
