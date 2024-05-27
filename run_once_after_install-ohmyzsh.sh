0d7bd4d7460abf07e98896b5108792faa36415fd
Rikai <rikaisensei@gmail.com>
2024-05-27 03:42:20 -0500

    Prefix is now a template variable

--- run_once_after_install-ohmyzsh.sh
+++ run_once_after_install-ohmyzsh.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-PREFIX="[CHEZMOI]"
-
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
-	echo "$PREFIX Getting ohmyz.sh"
+	echo "{{ .scriptPrefix }} Getting ohmyz.sh"
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc --skip-chsh
 	chsh -s /bin/zsh
 else
-	echo "$PREFIX .oh-my-zsh already found, skipping..."
+	echo "{{ .scriptPrefix }} .oh-my-zsh already found, skipping..."
 fi
