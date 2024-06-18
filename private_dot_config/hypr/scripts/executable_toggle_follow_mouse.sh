40342df93b5b984fed2d58e81f432ba746c13ea8
Rikai <rikaisensei@gmail.com>
2024-06-18 00:11:47 -0500

    add float switch override to script

--- private_dot_config/hypr/scripts/executable_toggle_follow_mouse.sh
+++ private_dot_config/hypr/scripts/executable_toggle_follow_mouse.sh
@@ -8,10 +8,12 @@ CURRENT_STATUS=$(hyprctl getoption -j input:follow_mouse | jq ".int")
 case $CURRENT_STATUS in
     0)
     sed -i 's/follow_mouse = 0/follow_mouse = 1/' $FILE
+    sed -i 's/float_switch_override_focus = 0/float_switch_override_focus = 1/' $FILE
     hyprctl notify 2 3000 0 "Follow Mouse ON"
     ;;
     1)
     sed -i 's/follow_mouse = 1/follow_mouse = 0/' $FILE
+    sed -i 's/float_switch_override_focus = 1/float_switch_override_focus = 0/' $FILE
     hyprctl notify 2 3000 0 "Follow Mouse OFF"
     ;;
 esac
