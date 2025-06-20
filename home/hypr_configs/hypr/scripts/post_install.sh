b138905b805e322d9bb63fa1e59f45d49044b015
Rikai <rikaisensei@gmail.com>
2025-06-19 22:28:03 -0500

    Change Chezmoi tag to RDM

--- home/hypr_configs/hypr/scripts/post_install.sh
+++ home/hypr_configs/hypr/scripts/post_install.sh
@@ -2,16 +2,16 @@ if [[ $XDG_CURRENT_DESKTOP == "Hyprland" ]]; then
     read -p "Do you want to setup Hyprland plugins now? (Make sure you are logged in into Hyprland) [y/N] " -n 1 -r
     echo
     if [[ $REPLY =~ ^[Yy]$ ]] then
-        echo "[CHEZMOI] Setting up Hyprland plugins..."
+        echo "[HYPRLAND] Setting up Hyprland plugins..."
         hyprpm update
         hyprpm add https://github.com/shezdy/hyprsplit # Add the plugin repository
         hyprpm enable hyprsplit # Enable the plugin
         hyprpm reload # Reload the plugins
 
         # Remove autoexecute
-        echo "[CHEZMOI] Cleaning up..."
+        echo "[HYPRLAND] Cleaning up..."
         sed -i "s/bash ~\/.config\/hypr\/scripts\/post_install.sh//" ~/.zshrc
         rm ~/.config/hypr/scripts/post_install.sh
-        echo "[CHEZMOI] Done!"
+        echo "[HYPRLAND] Done!"
     fi
 fi
