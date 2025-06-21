e4305e5b1129c383a86695345d3a22a51f67ded9
Rikai <rikaisensei@gmail.com>
2025-06-20 22:43:46 -0500

    Add missing theme settings for hyprland

--- home/setup-hyprland.sh
+++ home/setup-hyprland.sh
@@ -10,6 +10,10 @@ if ! [[ -e $HOME/Pictures/wallpapers/wallpaper1 ]]; then
     ln -s $WALLPAPER $HOME/Pictures/wallpapers/wallpaper2
 fi
 
+echo "[HYPRLAND] Setting up Theme..."
+gsettings set org.gnome.desktop.interface gtk-theme catppuccin-mocha-mauve-standard+default
+gsettings set org.gnome.desktop.interface icon-theme Tela-dark
+
 read -p "Do you want to install/replace the SDDM theme config? [y/N] " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]] then
