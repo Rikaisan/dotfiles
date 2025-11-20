49ff5dda281d8721ca7b41ff67fd439934ac7de8
Rikai <rikaisensei@gmail.com>
2025-11-20 00:47:13 -0500

    Add machine specific niri options

--- home/environments/rdm-niri.lua
+++ home/environments/rdm-niri.lua
@@ -1,10 +1,26 @@
+local function removeSection(text, tag)
+    return text:gsub("//~" .. tag ..".*//~end%-" .. tag, "")
+end
+
 function RDM_AddModules()
     local extraModules = { "wm-common" }
     return IsSet("extras") and extraModules or {}
 end
 
 function RDM_GetFiles()
-    return {
-        [".config/niri"] = Directory("niri")
-    }
+    local returnedFiles = {}
+
+    local function addCustomConfig(path, executable)
+        local cfg = Read("niri/" .. path)
+        cfg = removeSection(cfg, FlagIsSet("laptop") and "desktop" or "laptop")
+        cfg = removeSection(cfg, FlagIsSet("es") and "en" or "es")
+        if executable then
+            cfg = cfg:exec()
+        end
+        returnedFiles[".config/niri/" .. path] = cfg
+    end
+
+    addCustomConfig("config.kdl")
+
+    return returnedFiles
 end
