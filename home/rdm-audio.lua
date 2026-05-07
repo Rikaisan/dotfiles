e6d4257f06d3f61b73d3dbd6f3d4d2e5cc4234e6
Rikai <rikaisensei@gmail.com>
2026-05-06 23:54:24 -0500

    Add easy effects config to audio module

--- home/rdm-audio.lua
+++ home/rdm-audio.lua
@@ -1,6 +1,7 @@
 function RDM_GetFiles()
     return {
         [".config/rikai/home/config/rikai/carla-desktop.carxp"] = File("config/rikai/home/config/rikai/carla-desktop.carxp"),
+        [".var/app/com.github.wwmm.easyeffects"] = Directory("apps/easyeffects"),
         [".config/pipewire"] = Directory("config/pipewire")
     }
 end
