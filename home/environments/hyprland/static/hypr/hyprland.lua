24131163b31e0f58d5dcfba1f8f7a0eb215bc044
Rikai <rikaisensei@gmail.com>
2026-05-15 16:31:23 -0500

    Add keybind to change monitor refreshrate

--- home/environments/hyprland/static/hypr/hyprland.lua
+++ home/environments/hyprland/static/hypr/hyprland.lua
@@ -26,6 +26,19 @@ require("autostart")
 if MACHINE == "desktop" then
     hl.monitor({ output = "DP-1", mode ="1920x1080@144", position= "0x0", scale = "1" } )
     hl.monitor({ output = "HDMI-A-1", mode ="1920x1080@60", position= "1920x0", scale = "1" } )
+
+    -- Refresh Rate Swappers
+    hl.bind("SUPER + F1", function ()
+        if MACHINE == "desktop" then
+            hl.monitor({ output = "DP-1", mode ="1920x1080@144", position= "0x0", scale = "1" } )
+        end
+    end)
+
+    hl.bind("SUPER + F2", function ()
+        if MACHINE == "desktop" then
+            hl.monitor({ output = "DP-1", mode ="1920x1080@60", position= "0x0", scale = "1" } )
+        end
+    end)
 elseif MACHINE == "laptop" then
     hl.monitor({ output = "eDP-1", mode ="1920x1080@60", position= "0x0", scale = "1" } )
 end
