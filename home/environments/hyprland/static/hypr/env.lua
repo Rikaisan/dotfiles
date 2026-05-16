72a6be611bbbb337757d359ec038fc2bb26bcec0
Rikai <rikaisensei@gmail.com>
2026-05-16 17:13:15 -0500

    Add XDG vars to Hyprland

--- home/environments/hyprland/static/hypr/env.lua
+++ home/environments/hyprland/static/hypr/env.lua
@@ -15,3 +15,7 @@ hl.env("XCURSOR_THEME", "Adwaita")
 hl.env("XCURSOR_SIZE", "24")
 
 hl.env("GRIMBLAST_EDITOR", "swappy -f")
+
+hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
+hl.env("XDG_SESSION_TYPE", "wayland")
+hl.env("XDG_SESSION_DESKTOP", "Hyprland")
